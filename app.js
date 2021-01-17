6d599b6933cd8a4165c0a853aad956563ac73013
vineet <vineetsinghal2312@gmail.com>
2021-01-17 20:33:21 +0530

    dox

--- app.js
+++ app.js
@@ -2,35 +2,27 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtinput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function getTranslationURL(input)
-{
- return   serverURL + "?" + "text=" + input
+function getTranslationURL(input) {
+  return serverURL + "?" + "text=" + input;
 }
 
 function errorHandler(error) {
-    console.log("error occured" , error);
-    alert("there is something wrong in the server please tyr fter some time")
+  console.log("error occured", error);
+  alert("there is something wrong in the server please tyr fter some time");
 }
 
-function clickHandler()
-{
-    var textInput = txtinput.Value
+function clickHandler() {
+  var textInput = txtinput.value;
 
-
-fetch(getTranslationURL(textInput))
-.then(response => response.json())
-.then(json =>
-    {
-        var traslatedText = json.contents.translated
-        outputDiv.innerText = traslatedText
+  fetch(getTranslationURL(textInput))
+    .then((response) => response.json())
+    .then((json) => {
+      var traslatedText = json.contents.translated;
+      outputDiv.innerText = traslatedText;
     })
-    .catch(errorHandler)
+    .catch(errorHandler);
 }
 
-btnTranslate.addEventListener("click", clickHandler)
-
-
-
-
+btnTranslate.addEventListener("click", clickHandler);
